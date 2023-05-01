fdf3c5dc353c436b221ee3a733ba511566d64782
TheFourthLion <the00110100lion@gmail.com>
2023-04-30 19:11:48 -0700

    scape all of the game data

--- scrapper/game_data.js
+++ scrapper/game_data.js
@@ -122,45 +122,45 @@ const date = new Date().toLocaleDateString("en-us", {
         return listOfItems;
       });
 
-      // --------------------------------------------------- get game info titles -----------------------------
-      const gameInfoTitle = await page.evaluate(() => {
-        const listOfItems = [];
-        const length = document.querySelectorAll(
-          "#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr"
-        ).length;
-        console.log(`Length: ${length}`);
-        for (let i = 1; i < length; i++) {
-          try {
-            const data = document.querySelector(
-              `#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr:nth-child(${i}) > td:nth-child(1)`
-            ).innerHTML;
-            if (data != '<hr style="margin:1px">') {
-              listOfItems.push(data);
-            }
-          } catch (e) {}
-        }
-        return listOfItems;
-      });
+      // // --------------------------------------------------- get game info titles -----------------------------
+      // const gameInfoTitle = await page.evaluate(() => {
+      //   const listOfItems = [];
+      //   const length = document.querySelectorAll(
+      //     "#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr"
+      //   ).length;
+      //   console.log(`Length: ${length}`);
+      //   for (let i = 1; i < length; i++) {
+      //     try {
+      //       const data = document.querySelector(
+      //         `#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr:nth-child(${i}) > td:nth-child(1)`
+      //       ).innerHTML;
+      //       if (data != '<hr style="margin:1px">') {
+      //         listOfItems.push(data);
+      //       }
+      //     } catch (e) {}
+      //   }
+      //   return listOfItems;
+      // });
 
-      // --------------------------------------------------- get game info descriptions -----------------------------
-      const gameInfoDescription = await page.evaluate(() => {
-        const listOfItems = [];
-        const length = document.querySelectorAll(
-          "#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr"
-        ).length;
-        console.log(`Length: ${length}`);
-        for (let i = 1; i < length; i++) {
-          try {
-            const data = document.querySelector(
-              `#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr:nth-child(${i}) > td:nth-child(3)`
-            ).innerText;
-            if (data != '<hr style="margin:1px">') {
-              listOfItems.push(data);
-            }
-          } catch (e) {}
-        }
-        return listOfItems;
-      });
+      // // --------------------------------------------------- get game info descriptions -----------------------------
+      // const gameInfoDescription = await page.evaluate(() => {
+      //   const listOfItems = [];
+      //   const length = document.querySelectorAll(
+      //     "#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr"
+      //   ).length;
+      //   console.log(`Length: ${length}`);
+      //   for (let i = 1; i < length; i++) {
+      //     try {
+      //       const data = document.querySelector(
+      //         `#main > div.innerMain > div > div.mainContent > div.mainContent > div:nth-child(1) > table > tbody > tr:nth-child(${i}) > td:nth-child(3)`
+      //       ).innerText;
+      //       if (data != '<hr style="margin:1px">') {
+      //         listOfItems.push(data);
+      //       }
+      //     } catch (e) {}
+      //   }
+      //   return listOfItems;
+      // });
 
       // --------------------------------------------------- get reviewName description -----------------------------
       // const reviewName = await page.evaluate(() => {
@@ -177,6 +177,222 @@ const date = new Date().toLocaleDateString("en-us", {
       //   }
       //   return length;
       // });
+
+      const players = await page.evaluate(() => {
+        const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+          (row) => row.querySelector("td").textContent === "Players"
+        );
+
+        const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+        return data;
+      });
+
+      const year = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Year"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const cartSize = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Cart size"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const publisher = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Publisher"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const graphicsReview = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Graphics"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const soundReview = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Sound"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const gameplayReview = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Gameplay"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const overallReview = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Overall"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const CRC = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "CRC"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const MD5 = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "MD5"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const SHA1 = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "SHA1"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const verified = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Verified"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const version = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Version"
+          );
+
+          const data = row
+            .querySelector("td:nth-child(3)")
+            .textContent.trim()
+            .substring(0, 10);
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const serial = await page.evaluate(() => {
+        try {
+          const row = Array.from(document.querySelectorAll(".rounded tr")).find(
+            (row) => row.querySelector("td").textContent === "Serial #"
+          );
+
+          const data = row.querySelector("td:nth-child(3)").textContent.trim();
+
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const region = await page.evaluate(() => {
+        try {
+          const img = document.querySelector(
+            "tbody tr:nth-child(1) td:nth-child(3) img"
+          );
+          let data = img.getAttribute("title");
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
+
+      const gameFileName = await page.evaluate(() => {
+        try {
+          const data = document.querySelector("span#data-good-title").innerText;
+          return data;
+        } catch (e) {
+          return "Coming Soon";
+        }
+      });
 
       const reviewName = ["", ""];
       const reviewDate = ["", ""];
@@ -215,9 +431,25 @@ const date = new Date().toLocaleDateString("en-us", {
         cheatCodeDescription: cheatCodeDescription,
         reviewName: reviewName,
         reviewDate: reviewDate,
-        gameInfoTitle: gameInfoTitle,
         reviewDescription: reviewDescription,
-        gameInfoDescription: gameInfoDescription,
+        gameFileName: gameFileName,
+        region: region,
+        serial: serial,
+        version: version,
+        verified: verified,
+        SHA1: SHA1,
+        MD5: MD5,
+        CRC: CRC,
+        overallReview: overallReview,
+        gameplayReview: gameplayReview,
+        soundReview: soundReview,
+        graphicsReview: graphicsReview,
+        publisher: publisher,
+        cartSize: cartSize,
+        year: year,
+        players: players,
+        // gameInfoTitle: gameInfoTitle,
+        // gameInfoDescription: gameInfoDescription,
         oldDownloadLink: oldDownloadLink,
         downloadLink: downloadLink,
         timestamp: date,
@@ -225,7 +457,6 @@ const date = new Date().toLocaleDateString("en-us", {
 
       // ---------------------------- print info on game being scrapped downloaded ----------------------
       console.log("---------------------------------------");
-
       console.log(`Game Title: ${title}`);
       // console.log(`Download Size : ${downloadSize}`);
       console.log(`Console: ${gameConsole}`);
@@ -242,18 +473,18 @@ const date = new Date().toLocaleDateString("en-us", {
       // console.log(`Review Description: ${reviewDescription}`);
       console.log(`Games Left : ${lastGame - num}  |  GAME#${num}`);
 
-      await axios({
-        method: "POST",
-        url: `http://localhost:3002/api/games/create`,
-        data: games,
-        timeout: 7000,
-      })
-        .then(() => {
-          console.log("Successfully wrote data to db ✅");
-        })
-        .catch((error) => {
-          console.error({ "Failed writing to db 🛑": error });
-        });
+      // await axios({
+      //   method: "POST",
+      //   url: `http://localhost:3002/api/games/create`,
+      //   data: games,
+      //   timeout: 7000,
+      // })
+      //   .then(() => {
+      //     console.log("Successfully wrote data to db ✅");
+      //   })
+      //   .catch((error) => {
+      //     console.error({ "Failed writing to db 🛑": error });
+      //   });
     } catch (e) {
       const missingGame = `Missing game #${num}`;
       missingGames.push(missingGame);
