349b4e9b2398a6df85093cf4e04f4143a05ca701
TheFourthLion <the00110100lion@gmail.com>
2023-06-17 09:47:14 -0700

    add emulator page

---
+++ client/pages/emulators.js
@@ -0,0 +1,1 @@
+import React from "react"; const emulators = () => { return ( <div className="emulators"> <div className="container"> <h1 className="content-header">emulators</h1> </div> </div> ); }; export default emulators;
