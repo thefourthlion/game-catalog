d3a954e0e4e590927df9a61a02a3d21b3bdcfb88
TheFourthLion <the00110100lion@gmail.com>
2023-06-17 14:42:54 -0700

    add footer info and styles

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="Footer">
       <div className="container">
-        <h1 className="content-header">Footer</h1>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">Consoles</Link>
+          </li>
+          <li>
+            <Link href="/">Handhelds</Link>
+          </li>
+          <li>
+            <Link href="/">Emulators</Link>
+          </li>
+          <li>
+            <Link href="/">Emulators</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
